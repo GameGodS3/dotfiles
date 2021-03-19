0b9a3ab52a569c268e4c0d93d8ed9dfb3e12993e
GameGodS3 <sudevssuresh@gmail.com>
2021-03-19 19:26:53 +0530

    Updated qrcodegen python script to handle http prefix

--- qrcodegen.py
+++ qrcodegen.py
@@ -3,6 +3,10 @@ import sys
 import pyqrcode
 
 def QRgen(text):
+    if (text[0:7]=="http://"):
+        text = text[7:]
+    elif (text[0:8]=="https://"):
+        text = text[8:]
     text = "http://"+text
     url = pyqrcode.create(text)
     url.png('localhost.png', scale = 6) 
